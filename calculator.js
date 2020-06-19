d94d7e2a7cc762d7ad22e92ea04ffb1ed3f5565e
Phu Pham <phupham07@yahoo.com>
2020-06-18 19:47:32 -0700

    added function math operators

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const addition = (a, b) => {return a + b;};
+
+const subtraction = (a, b) => {return a - b;};
+
+const multiplication = (a, b) => {return a * b;};
+
+const divison = (a, b) => {return a / b;};
