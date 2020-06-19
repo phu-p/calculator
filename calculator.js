8085120df28671262270d47734e6a6beb2435d31
Phu Pham <phupham07@yahoo.com>
2020-06-18 19:48:30 -0700

    add function operate

--- calculator.js
+++ calculator.js
@@ -5,3 +5,22 @@ const subtraction = (a, b) => {return a - b;};
 const multiplication = (a, b) => {return a * b;};
 
 const divison = (a, b) => {return a / b;};
+
+const operate = (operator, a, b) => {
+    switch(operator) {
+        case '+':
+            addition(a, b);
+            break;
+        case '-':
+            subtraction(a, b);
+            break;
+        case '*':
+            multiplication(a, b);
+            break;
+        case '/':
+            divison(a, b);
+            break;
+        default:
+            return "SYNTAX ERROR";
+    };
+};
