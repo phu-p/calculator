3227e68f49584d3997af82c8ea601843e7ad924d
Phu Pham <phupham07@yahoo.com>
2020-07-10 18:56:37 -0700

    add factorial and power functions

--- calculator.js
+++ calculator.js
@@ -6,6 +6,17 @@ const multiplication = (a, b) => {return a * b;};
 
 const divison = (a, b) => {return a / b;};
 
+const power = (a, b) => {return Math.pow(a, b)};
+
+function factorial(number) {
+	var accumulator = 1;
+	var limit = 0
+	for (var i = number; i > limit; --i) {
+		accumulator *= i;
+	};
+	return accumulator;
+};
+
 const operate = (operator, a, b) => {
     switch(operator) {
         case '+':
