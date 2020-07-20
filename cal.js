639a98f71f97c7b7ffd749f4c31c84bbb7ddfcd8
Phu Pham <phupham07@yahoo.com>
2020-07-19 21:54:11 -0700

    test out the function of button by using click event and receive the feedback by console log

--- cal.js
+++ cal.js
@@ -41,3 +41,12 @@ const operate = (operator, a, b) => {
             return "SYNTAX ERROR";
     };
 };
+
+let test = element => console.log(element);
+
+let calculator = document.getElementsByClassName("calculator");
+let button = document.getElementsByClassName("button");
+
+for(let i = 0; i < button.length; ++i) {
+    button[i].addEventListener("click", test);
+}
